3cb15230ea1f9b39ebbf138c5d07e297b3a878d8
rigdent <rigdent@carleton.edu>
2014-06-05 21:08:47 -0500

    Revert 8451605..67b231e
    This rolls back to commit 8451605b20123bf015c361135c139f63c48064d4.

--- backPropLearning.py
+++ backPropLearning.py
@@ -4,6 +4,7 @@ Tenzin Rigden and Christopher Winter
 Uses the backwards propogation algorithm to build a neural network. This particular code is designed to be used for optical character recognition.
 '''
 
+import math
 
 class inputNode:
     def __init__(self,inputValue):
@@ -20,11 +21,22 @@ Still need to implement the calculate output and calculate input functions.
 '''
 class hiddenNode:
     def __init__(self,numInputs):
-        self.weights = [0] * numWeights
+        self.weights = [0] * numInputs
         self.weights[0] = 1
-        self.inputs = [0] * numWeights
+        self.inputs = [0] * numInputs
         self.inputs[0] = 1
         self.output = 0
+<<<<<<< HEAD
+        self.delta = 0
+
+    def setDelta(self, newDelta):
+        self.delta = newDelta
+        
+    def getDelta(self):
+        return self.delta
+=======
+        self.weightedInput = 0
+>>>>>>> FETCH_HEAD
 
     def setWeight(self,weight,wieghtIndex):
         self.weights[weightIndex] = weight
@@ -42,10 +54,20 @@ class hiddenNode:
             self.setInput(inputs[i],i)
 
     def calculateWeightedInputs(self):
-        return 1
+        weightedInput = 0 # This is the inj notation in the book
+        for i in range(len(self.weights)):
+            weightedInput = weightedInput + (self.weights[i] * self.inputs[i])
+        self.weightedInput = weightedInput
+        return weightedInput
+
+    def getWeightedInput(self):
+        return self.weightedInput
 
     def calculateOutput(self):
         return 1
+
+    def setOutput(self,output):
+        self.output = output
 
     def getWeights(self):
         return self.weights
@@ -63,6 +85,17 @@ class outputNode:
         self.inputs = [0] * numHiddenNodes
         self.input[0] = 1
         self.output = 0
+<<<<<<< HEAD
+        self.delta = 0
+
+    def setDelta(self, newDelta):
+        self.delta = newDelta
+        
+    def getDelta(self):
+        return self.delta
+=======
+        self.weightedInput = 0
+>>>>>>> FETCH_HEAD
 
     def setWeight(self,weight,wieghtIndex):
         self.weights[weightIndex] = weight
@@ -80,16 +113,32 @@ class outputNode:
         self.inputs[inputIndex] = input
 
     def calculateWeightedInputs(self):
-        return 1
+        weightedInput = 0 # This is the inj notation in the book
+        for i in range(len(self.weights)):
+            weightedInput = weightedInput + (self.weights[i] * self.inputs[i])
+        self.weightedInput = weightedInput
+        return weightedInput
+
+    def getWeightedInput(self):
+        return self.weightedInput
 
     def calculateOutput(self):
         return 1
+
+    def setOutput(self,output):
+        self.output = output
 
     def getWeights(self):
         return self.weights
 
     def getInputs(self):
         return self.inputs
+
+
+def sigmoidFunction(x):
+    return 1 / (1 + math.exp(x))
+
+def sigmoidDerivativeFunction(x):
 
 
 def setInitialWeights(network):
@@ -102,6 +151,8 @@ def setInitialWeights(network):
         node.batchSetWeights(initialWeightsOutputs)
 
     return None
+
+def 
 
 '''
 Main algorithm.
@@ -127,7 +178,13 @@ def backPropLearning(examples,network):
 
             for node in hidden:
                 for inputNumber in range(len(inputs)):
-                    node.setInput(inputs[inputNumber],inputNumber+1)
+                    node.setInput(inputs[inputNumber],inputNumber+1) # The "+1" is because of the dummy input
+                weightedInputs = node.calculateWeightedInputs()
+                output = sigmoidFunction(weightedInputs)
+                node.setOutput(output)
+
+
+
 
 
 
